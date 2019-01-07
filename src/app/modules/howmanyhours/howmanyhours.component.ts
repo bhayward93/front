00fddf89d1d5ee0c0d058c69ce32d8663568c81b
ben@gravitichain.com <ben@gravitichain.com>
2019-01-07 16:58:15 +0000

    Highlighting changes

--- src/app/modules/howmanyhours/howmanyhours.component.ts
+++ src/app/modules/howmanyhours/howmanyhours.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 import { Navigation as NavigationService } from '../../services/navigation';
 import { Session } from '../../services/session';
 import { MindsTitle } from '../../services/ux/title';
@@ -12,42 +11,48 @@ import { LoginReferrerService } from '../../services/login-referrer.service';
   templateUrl: 'howmanyhours.component.html'
 })
 
+/**
+ * Component displaying the amount of hours that a User has been registered with the site.
+ */
 export class HowManyHoursComponent {
-  private seconds : any;
+  protected minds = window.Minds;
+  protected hours : any;
   private user: any;
+
   constructor(
     public client: Client,
     public title: MindsTitle,
-    public session: Session
-
+    public session: Session,
+    public router: Router
   ) {
-  }
-  async ngOnInit() {
-    // if (this.session.isLoggedIn()) {
-    //   this.router.navigate(['/newsfeed']);
-    //   return;
-    // }
-    this.user = await this.session.getLoggedInUser(); //Get the current user.
-    this.seconds = await this.findDifference(this.user.account_time_created, + new Date()); //unary operator used to invoke Date's 'valueOf' function.
-    console.log(this.user.account_time_created)
-    console.log(+ new Date)
-
-    // this.seconds = await this.client.get('api/v2/howmanyhours')
-    
-    // this.seconds = response;
-    //         })
-    //         .catch(e => {
-    //           console.error('[Minds Admin] Cannot get account age.', e);
-    //         });
-    //console.log(this.account_time_created);
-    console.log(this.seconds)
     this.title.setTitle('How Many Hours?');
   }
 
+  async ngOnInit() {
+      if (!this.session.isLoggedIn()) { //If not logged in.
+        this.router.navigate(['/']); //Pass back to the router with '/'.
+        return; //Exit/
+      }
+      this.client.get('api/v2/howmanyhours') //Get data from endpoint via client
+        .then(async (response: any) => { 
+          let _seconds = await this.findDifference(response.seconds, + new Date() / 1000) // Unary operator used to invoke Date's 'valueOf' function.
+          this.hours = (_seconds / 60).toFixed(2) //2 DP for formatting.
+        })
+        .catch(e => {
+          console.error('[Minds Admin] Cannot get account age.', e);
+        });
+  }
+
+  /**
+   * Finds the difference between two given Numbers, independant of order. 
+   * Tolerates string input. 
+   * @param a - { Number | String } - The first number to be compared.
+   * @param a - { Number | String } - The seconds number to be compared.
+   * @returns - { Number } - The difference between the two given numbers.
+   */
   async findDifference(a, b){
     a = Number(a) 
     b = Number(b)
-    console.log(a,b)
     return (a > b) ? a - b : b - a;
   }
 }
